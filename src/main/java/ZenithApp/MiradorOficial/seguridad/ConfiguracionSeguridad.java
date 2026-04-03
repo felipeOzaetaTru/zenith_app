7e39145965b8f2cd6d3e38398857ef01a5c3c3f8
Felipe Ozaeta <hfozaetatru@gmail.com>
2026-04-03 16:47:14 -0500

    Cambiando configuracion de config 2

--- src/main/java/ZenithApp/MiradorOficial/seguridad/ConfiguracionSeguridad.java
+++ src/main/java/ZenithApp/MiradorOficial/seguridad/ConfiguracionSeguridad.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,13 +26,14 @@ public class ConfiguracionSeguridad {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults()) // ✅ esto conecta tu CorsConfig
                 .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/usuario/crear-usuario").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/iniciar-sesion").permitAll()
                         .anyRequest().authenticated()
                 )
-                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
